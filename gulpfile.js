621134b7673ace412cc284c714c1b77492073c79
Ryan Rampersad <ryan.rampersad@gmail.com>
2015-06-29 23:29:46 -0500

    Added vendor files: moment, react and zepto

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,33 @@ var gulp = require('gulp');
 var babel = require('gulp-babel');
 var concat = require('gulp-concat');
 
+// personally written javascript
 gulp.task('js', function(){
   var files = [
     'resources/js/app.js'
   ];
   return gulp.src(files)
-        .pipe(concat('all.js'))
+        .pipe(concat('app.js'))
         .pipe(babel())
         .pipe(gulp.dest('resources/build/js/'));
 });
 
-gulp.task('default', ['js']);
+// javascript written by third party vendors
+gulp.task('js-vendor', function(){
+  var files = [
+    'resources/js/vendor/react-with-addons.js',
+    'resources/js/vendor/moment.js',
+    'resources/js/vendor/zepto.js'
+  ];
+  return gulp.src(files)
+        .pipe(concat('vendor.js'))
+        .pipe(gulp.dest('resources/build/js/'));
+});
 
+// basic task: `gulp`
+gulp.task('default', ['js', 'js-vendor']);
+
+// watch task: `gulp watch`
 gulp.task('watch', function(){
   gulp.watch('resources/js/*', ['default']);
 });
